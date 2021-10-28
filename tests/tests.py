7c9a87eca5cdcee5baa6c0c781ec9488b59d5aff
kolazojar <mkrolbass@gmail.com>
2021-10-27 21:14:02 -0400

    updated tests

--- tests/tests.py
+++ tests/tests.py
@@ -14,6 +14,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--number-of-users', type=int, default=8)
 parser.add_argument('--number-of-user-tests', type=int, default=4)
 parser.add_argument('--use-server', action='store_true')
+parser.add_argument('--seed-only', action='store_true')
 args = parser.parse_args()
 
 
@@ -157,8 +158,9 @@ def main():
     k = args.number_of_user_tests
 
     # Access admin account.
-    admin = TestAdmin()
-    admin.get_token()
+    if not args.seed_only:
+        admin = TestAdmin()
+        admin.get_token()
 
     # Create user profiles.
     users = [ TestUser() for i in range(n) ]
@@ -176,43 +178,42 @@ def main():
         users[idx].new_tests()
         users[idx].update_profile()
 
-    print('Press any key to continue ...', end='')
-    input()
+    if not args.seed_only:
+        print('Press any key to continue ...', end='')
+        input()
 
-    data = admin.get_data()
+        data = admin.get_data()
 
-    if data is not None:
-        print('Found {} users who need to be evaluated.'.format(len(data)))
+        if data is not None:
+            print('Found {} users who need to be evaluated.'.format(len(data)))
 
-        cmt = admin.post_results(data)
+            cmt = admin.post_results(data)
 
-        print('Was able to update {} out of {} users fall risk.'.format(cmt['results_updated'], len(data)))
+            print('Was able to update {} out of {} users fall risk.'.format(cmt['results_updated'], len(data)))
 
-        user = users[idxs[random.randint(0, k - 1)]]
+            user = users[idxs[random.randint(0, k - 1)]]
 
-        sent = user.tests
+            sent = user.tests
 
-        test_passed = False
-        for i in range(len(data)):
-            if data[i]['email'] == user.email:
-                received = pickle.loads(base64.b64decode(data[i]['tests'].encode('utf-8')))
+            test_passed = False
+            for i in range(len(data)):
+                if data[i]['email'] == user.email:
+                    received = pickle.loads(base64.b64decode(data[i]['tests'].encode('utf-8')))
 
-                print('Sent tests are equal to the received tests? {}'.format('PASS' if sent == received else 'FAIL'))
+                    print('Sent tests are equal to the received tests? {}'.format('PASS' if sent == received else 'FAIL'))
 
-                print('Printing the rest of the data ...')
-                print()
+                    print('Printing the rest of the data ...')
+                    print()
 
-                data[0].pop('tests')
+                    data[0].pop('tests')
 
-                print(json.dumps(data[0], indent=2))
+                    print(json.dumps(data[0], indent=2))
 
-                test_passed = True
-                break
-        
-        if not test_passed:
-            print('User was not found in the data.')
-
-        user.delete_data()
+                    test_passed = True
+                    break
+            
+            if not test_passed:
+                print('User was not found in the data.')
 
 
 if __name__ == '__main__':
